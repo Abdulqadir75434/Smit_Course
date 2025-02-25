58790c3d32815a322bc43784fc3cf8e3ec8b4ce3
Abdulqadir75434 <aq987654325@gmail.com>
2025-02-25 09:45:03 -0800

    html and css code

---
+++ html-Css/js/app.js
@@ -0,0 +1,35 @@
+// console.log("Abdul qadir");
+// console.log("age 17");
+// console.log(true)
+
+// var city = "karachi"
+// console.log(city)
+ 
+alert('Hello world') 
+// var userName ="abdul qadir";
+// userName = "Waqas"
+// console.log(userName)
+
+// var userName ="abdul qadir";
+// console.log(userName)
+// var userName = "Waqas"; 
+// console.log(userName)
+
+
+
+// //percentage calculator
+
+// var totalMarks =400;
+
+// var UserName = prompt('enter your name')
+// var engmarks = +prompt("enter your English Marks")
+// var urdumarks = +prompt("enter your urde Marks")
+// var mathsmarks = +prompt("enter your Maths Marks")
+// var commarks = +prompt("enter your computer Marks")
+
+// var studentObtainedMarks =engmarks + urdumarks + mathsmarks + commarks;
+// var percentage = studentObtainedMarks / totalMarks * 100;
+// console.log("Username is " + UserName + 'and-result is' + percentage +"%")
+
+
+
